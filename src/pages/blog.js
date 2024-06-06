f36d515a6df13522affa68e5085200014fefb969
Joao-Miguel-Maia <80039901+Joao-Miguel-Maia@users.noreply.github.com>
2024-06-06 14:00:31 +0100

    commit cap5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,30 +6,36 @@ import { graphql } from 'gatsby'
 
 const BlogPage = ({ data }) => {
     return (
-        <Layout pageTitle="My Blog Posts">
-            <ul>
-                {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
-                    ))
-                }
-            </ul>
-        </Layout>
-    )
-}
+      <Layout pageTitle="My Blog Posts">
+        {
+          data.allMdx.nodes.map((node) => (
+            <article key={node.id}>
+              <h2>{node.frontmatter.title}</h2>
+              <p>Posted: {node.frontmatter.date}</p>
+              <p>{node.excerpt}</p>
 
-export const query = graphql`
-  query {
-    allFile {
-      nodes {
-        name
+            </article>
+          ))
+        }
+      </Layout>
+    )
+  }
+  
+  export const query = graphql`
+    query {
+      allMdx(sort: { frontmatter: { date: DESC }}) {
+        nodes {
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+          }
+          id
+          excerpt
+        }
       }
     }
-  }
-`
-
-export const Head = () => <Seo title="My Blog Posts" />
-
-export default BlogPage
+  `
+  
+  export const Head = () => <Seo title="My Blog Posts" />
+  
+  export default BlogPage
